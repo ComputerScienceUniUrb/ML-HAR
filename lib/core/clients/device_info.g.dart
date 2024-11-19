32c2be894657828916e0a6ca32f100ffeba79e67
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2024-11-19 09:41:01 +0100

    update version

---
+++ lib/core/clients/device_info.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'device_info.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getAndroidDeviceInfoHash() =>
+    r'34a1877bfd42badcbca00793cfe6d2fec488debe';
+
+/// See also [getAndroidDeviceInfo].
+@ProviderFor(getAndroidDeviceInfo)
+final getAndroidDeviceInfoProvider =
+    AutoDisposeFutureProvider<AndroidDeviceInfo>.internal(
+  getAndroidDeviceInfo,
+  name: r'getAndroidDeviceInfoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getAndroidDeviceInfoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetAndroidDeviceInfoRef
+    = AutoDisposeFutureProviderRef<AndroidDeviceInfo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
