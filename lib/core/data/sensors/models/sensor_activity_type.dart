35c9af4426f1852e1aadf00e66cd35e57d59127e
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2025-03-03 15:40:28 +0100

    update activity recognition

--- lib/core/data/sensors/models/sensor_activity_type.dart
+++ lib/core/data/sensors/models/sensor_activity_type.dart
@@ -12,3 +12,20 @@ enum SensorActivityType {
   walkingCircle,
   run,
 }
+
+// • Camminata;
+// • camminata sul posto;
+// • corsa;
+// • corsa sul posto;
+// • rotazione delle braccia (“avanti – indietro” e/o “indietro - avanti”);
+// • cyclette;
+// • tapiroulanne.
+
+
+// cyclette: resistenza, standard o recumbent
+// tapiroulant: pendenza, velocità
+
+// campionamento
+// tagliare i primi 5 secondi di attività
+// bip finale con un secondo di ritardo per tagliare la fine della traccia
+
--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -3,14 +3,14 @@ import 'package:flutter_activity_recognition/flutter_activity_recognition.dart';
 
 Future<bool> isPermissionGrants() async {
   // Check if the user has granted permission. If not, request permission.
-  PermissionRequestResult reqResult;
+  ActivityPermission reqResult;
   reqResult = await FlutterActivityRecognition.instance.checkPermission();
-  if (reqResult == PermissionRequestResult.PERMANENTLY_DENIED) {
+  if (reqResult == ActivityPermission.PERMANENTLY_DENIED) {
     logger.d('Permission is permanently denied.');
     return false;
-  } else if (reqResult == PermissionRequestResult.DENIED) {
+  } else if (reqResult == ActivityPermission.DENIED) {
     reqResult = await FlutterActivityRecognition.instance.requestPermission();
-    if (reqResult != PermissionRequestResult.GRANTED) {
+    if (reqResult != ActivityPermission.GRANTED) {
       logger.d('Permission is denied.');
       return false;
     }
